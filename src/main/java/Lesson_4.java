87fac1ed94cc7771a4a1976b01beba73cb044831
Nikita <kostyukov.nikita1999@yandex.ru>
2024-04-21 23:37:15 +0300

    fix task №10

--- src/main/java/Lesson_4.java
+++ src/main/java/Lesson_4.java
@@ -99,7 +99,7 @@ public class Lesson_4 {
         for (int i = 0; i < array.length; i++) {
             if (array[i] == 0) {
                 array[i] = 1;
-            } else if (array[i] == 1) {
+            } else {
                 array[i] = 0;
             }
         }
