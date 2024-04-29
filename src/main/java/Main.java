c5d67024c4fb004aa5e984518482570c48fa1da1
Nikita <kostyukov.nikita1999@yandex.ru>
2024-04-29 14:29:54 +0300

    code review

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,26 +4,19 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-
-        //2.1 инициализация массива сотрудников
         Employee[] employees = new Employee[5];
         employees[0] = new Employee("Vasya", "java developer", "vasya@mail.ru", "375442345678", 2300, 22);
         employees[1] = new Employee("Kolya", "IOS developer", "kolya@mail.ru", "375442345678", 2000, 29);
         employees[2] = new Employee("Gena", "python developer", "gena@mail.ru", "375443456789", 3500, 19);
         employees[3] = new Employee("Sasha", "c++ developer", "sasha@mail.ru", "375292485710", 4000, 40);
         employees[4] = new Employee("Petya", "designer", "petya@mail.ru", "375440987654", 1500, 34);
-
-        //2.2 Вывод массива сотрудников в консоль
         for (Employee employee : employees) {
             System.out.println(employee.getInfo());
         }
 
-
-        //3 Демонстрация работы 3 задания
         Park.Attraction park = new Park("Румянцевых-Паскевичей").new Attraction("Веселый аттракцион", "8:00-17:00",25);
         System.out.println(park.getName());
         System.out.println(park.getPrice());
         System.out.println(park.getTime());
-
     }
 }
